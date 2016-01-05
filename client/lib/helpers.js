de7d80cfc776af89a8f751a9db97a97329038fc3
hhalka <hmakarukha@gmail.com>
2016-01-05 20:32:41 +0000

    Initial commit

---
+++ client/lib/helpers.js
@@ -0,0 +1,28 @@
+Template.body.helpers({
+    queries: [
+        { 
+            term: "Restaurants",
+            latitude: 34.345454,
+            longitude: 41.8723,
+            radius: 5,
+            date: new Date ("Sep 23 13:09"),
+        },
+        {
+            term: "Museums",
+            latitude: 139.71038,
+            longitude: 12.4801, 
+            radius: 2,
+            date: new Date ("Sep 23 13:10"),
+        }
+    ],
+    formatDate: function(date) {
+        return moment(date).format("MMM DD HH:mm");
+    }
+});
+
+Template.query.helpers({
+    formatDate: function(date) {
+        return moment(date).format("MMM DD HH:mm");
+    }
+});
+
